86dedd162615c2d4cbea85fcf3e99161fe8ba7f4
DanielaMazur <danielamazur2000@gmail.com>
2021-09-19 10:12:42 +0300

    init server

---
+++ DiningHallServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DiningHallServer
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               HTTPServer server = new(3000);
+               server.Start();
+
+          }
+     }
+}
